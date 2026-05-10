e8d39e3cc06941ad6ba34f4821d6a132df373286
Tivins <pub.git@elabo.fr>
2026-05-10 22:47:59 +0200

    move files

--- examples/run.bat
+++
@@ -1,4 +0,0 @@
-
-set LLM_MODEL=../models/google_gemma-4-E4B-it-Q6_K.gguf
-
-llama-server.exe -m %LLM_MODEL% --port 8080 --no-webui
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-LLM_MODEL=../models/google_gemma-4-E4B-it-Q6_K.gguf
+LLM_MODEL=./models/google_gemma-4-E4B-it-Q6_K.gguf
 
 llama-server -m $LLM_MODEL --port 8080 --no-webui
